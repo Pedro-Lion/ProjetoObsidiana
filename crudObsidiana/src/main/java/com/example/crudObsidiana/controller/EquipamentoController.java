6ccba3c4ce519cbf4ef6fc0499408a3c0e329fb6
Pedro-Lion <pedro.santos033@sptech.school>
2025-09-09 21:23:45 -0300

    Crud funcionando update e delete

--- crudObsidiana/src/main/java/com/example/crudObsidiana/controller/EquipamentoController.java
+++ crudObsidiana/src/main/java/com/example/crudObsidiana/controller/EquipamentoController.java
@@ -21,6 +21,41 @@ public class EquipamentoController {
     @GetMapping
     public List<Equipamento> listarTodos(){return repository.findAll();}
 
+//    @GetMapping("/{id}")
+//    public Equipamento selecionar(@PathVariable("id") Long id) {
+//        Equipamento equipamento = repository.findById(id).orElse(null);
+//        return equipamento;
+//    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Equipamento> recuperar(@PathVariable("id") Long id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Equipamento> excluir(@PathVariable("id") Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Equipamento> atualizar(@PathVariable("id") Long id, @RequestBody
+    Equipamento equipamento) {
+        if (repository.existsById(id)) {
+            equipamento.setId(id);
+            repository.save(equipamento);
+            return ResponseEntity.ok(equipamento);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+
 
 
     private final EquipamentoRepository repository;
