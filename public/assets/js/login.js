4718188dc3f48991afbab29f307f4daf6c42a921
mikki-unix <mikki.anjos@sptech.school>
2025-09-13 20:21:45 -0300

    [json-server] js para cadastro e js.utils

---
+++ public/assets/js/login.js
@@ -0,0 +1,19 @@
+import * as u from "./utils.js";
+
+document.getElementById("btn_cadastrar").addEventListener("click", (e) => {
+  e.preventDefault();
+  cadastrar();
+})
+
+function cadastrar() {
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  const confirmarSenha = document.getElementById("confirmar_senha").value;
+
+  if(u.alertarErro({nome, email, senha, confirmarSenha})) return;
+
+  u.fazerRequisicao("POST", {
+    nome, email, senha
+  });
+}
---
+++ public/assets/js/utils.js
@@ -0,0 +1,43 @@
+const urlBase = "http://localhost:3000/usuarios"
+
+async function fazerRequisicao(metodo = "", corpo = {}) {
+  if (metodo == "") metodo = "GET"
+  else metodo = metodo.toUpperCase();
+
+  try {
+    const request = await fetch(urlBase, {
+      method: metodo,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(corpo)
+    });
+
+    const dados = await request.json();
+
+    return dados;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function alertarErro(dados = {}) {
+  if(!emailValido(dados.email)) {
+    alert("O email digitado é inválido.");
+    return true;
+  }
+  if(!senhasCoincidem(dados.senha, dados.confirmarSenha)) {
+    alert("As senhas não coincidem.");
+    return true;
+  }
+}
+
+function emailValido(email = "") {
+  return email.includes("@");
+}
+
+function senhasCoincidem(senha = "", confirmarSenha = "") {
+  return senha == confirmarSenha;
+}
+
+export { fazerRequisicao, alertarErro }
