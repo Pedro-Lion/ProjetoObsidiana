b1593741504ee733a1170d932432b89a0e0e1c4d
shell-y <shelly.nadudvari@sptech.school>
2025-10-12 19:41:37 -0300

    componente botão: adicionando borda e tirando fundo

--- src/components/Buttons/BotaoBordaGradiente.jsx
+++ src/components/Buttons/BotaoBordaGradiente.jsx
@@ -1,17 +1,15 @@
-export function BotaoBordaGradiente(){
-    return(
-        <div className="bg-slate-100 mx-auto flex h-screen w-screen items-center justify-center">
-            <button type="button" className="h-12 w-36 group transition ease-in-out duration-300
-            flex items-center justify-center rounded-full
+export function BotaoBordaGradiente() {
+    return (
+        <button type="button" className="h-12 w-36 group transition ease-in-out duration-300
+            flex items-center justify-center rounded-full my-3.5
             bg-gradient-to-l from-fuchsia-400 via-indigo-500 to-green-300
             text-white
             hover:text-gray-950 hover:shadow-sm hover:shadow-indigo-200 hover:p-[.15rem]">
-                <div className="rounded-full w-full h-full
+            <div className="rounded-full w-full h-full
                 flex items-center justify-center
                 bg-slate-950 group-hover:bg-slate-100
                 group-hover:transition group-hover:ease-in-out group-hover:duration-300">
-                    Clique aqui</div>
-            </button>
-        </div>
+                Clique aqui</div>
+        </button>
     )
 }
