c632069c5aedbeee5e62e646baa29ccfb7f332b0
shell-y <shelly.nadudvari@sptech.school>
2025-10-12 23:17:02 -0300

    alteração botao gradiente

--- src/components/Buttons/BotaoBordaGradiente.jsx
+++ src/components/Buttons/BotaoBordaGradiente.jsx
@@ -7,7 +7,7 @@ export function BotaoBordaGradiente() {
             hover:text-gray-950 hover:shadow-sm hover:shadow-indigo-200 hover:p-[.15rem]">
             <div className="rounded-full w-full h-full
                 flex items-center justify-center
-                bg-slate-950 group-hover:bg-slate-100
+                bg-slate-950 group-hover:bg-white
                 group-hover:transition group-hover:ease-in-out group-hover:duration-300">
                 Clique aqui</div>
         </button>
