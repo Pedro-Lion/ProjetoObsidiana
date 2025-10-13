a2513f9bbb607fb61d16d985f0619727fa8bacb6
shell-y <shelly.nadudvari@sptech.school>
2025-10-13 00:08:08 -0300

    alterações botao gradiente

--- src/components/Buttons/BotaoBordaGradiente.jsx
+++ src/components/Buttons/BotaoBordaGradiente.jsx
@@ -1,18 +1,18 @@
-export function BotaoBordaGradiente({titulo="Clique aqui"}) {
+export function BotaoBordaGradiente({ titulo = "Clique aqui" }) {
     return (
         <button type="button" className="h-12 min-w-36
         group transition ease-in-out duration-300
             flex items-center justify-center rounded-full my-3.5
             bg-gradient-to-l from-fuchsia-400 via-indigo-500 to-green-300
-            text-white
+            text-white text-[1.1rem]
             hover:text-gray-950 hover:shadow-sm hover:shadow-indigo-200 hover:p-[.15rem]">
             <div className="rounded-full w-full h-full
-            leading-none px-2.5
+            leading-none px-3.5
                 flex items-center justify-center
                 bg-slate-950 group-hover:bg-white
                 group-hover:transition group-hover:ease-in-out group-hover:duration-300">
                 {titulo}
-                </div>
+            </div>
         </button>
     )
 }
