ab7cf9766c2fe1169f39243a06e3d576b6750695
shell-y <shelly.nadudvari@sptech.school>
2025-10-13 01:48:42 -0300

    ajustes app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import { Outlet } from "react-router-dom";
 
 export function App() {
   return (
-    <>
+    <div className="
+    flex
+    flex-row
+    items-center
+    overflow-hidden
+    bg-gradient-to-t from-violet-400 via-indigo-300 via-40% to-slate-50 backdrop-blur-lg
+    ">
       <header
         style={{ height: "90vh" }}
-        className="w-114 p-6 fixed -translate-y-1/2 top-1/2 left-4 rounded-[3.2rem] bg-gray-800 text-gray-50"
+        className="w-114 p-6 fixed -translate-y-1/2 top-1/2 left-4 rounded-[2rem] bg-zinc-900 text-gray-50"
       >
         <img className="h-10" src="/logo.png" alt="Logo Obsidiana" />
 
@@ -17,6 +23,6 @@ export function App() {
       <main className="pl-123">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
