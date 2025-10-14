37d30ed355709677f1388c842b20a6c9f38b18b3
mikki-unix <mikki.anjos@sptech.school>
2025-10-14 17:40:19 -0300

    inputs - refatoração

--- src/components/Inputs/InputCheckbox.jsx
+++ src/components/Inputs/InputCheckbox.jsx
@@ -1,22 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export function InputCheckbox(props) {
-    const [isChecked, setIsChecked] = useState(false);
-    const handleCheckboxChange = (event) => {
-        setIsChecked(event.target.checked);
-    };
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked);
+  };
 
-    return (
-        <div className="flex flex-row w-fit items-center">
-            <input
-                type="checkbox"
-                checked={isChecked}
-                onChange={handleCheckboxChange}
-                className="accent-violet-200 hover:accent-violet-400 hover:cursor-pointer"/>
-            <label className="w-fit h-auto pl-2
-            text-slate-700 font-normal text-[1.2rem]">
-            {props.texto}
-            </label>
-        </div>
-    );
+  return (
+    <div className={"flex flex-row items-center" + props.className}>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+        className="accent-violet-200 hover:accent-violet-400 hover:cursor-pointer"
+      />
+      <label
+        className="w-fit h-auto pl-2
+        text-slate-700 font-normal text-[1.2rem]"
+      >
+        {props.texto}
+      </label>
+    </div>
+  );
 }
