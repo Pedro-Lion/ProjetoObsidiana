fd0922b4f1b29072606bc918578f6bbecb4e05f7
shell-y <shelly.nadudvari@sptech.school>
2025-10-14 18:20:39 -0300

    ajustando padding

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export function App() {
   return (
     <>
       <header
-        className="w-114 py-6 flex-none text-gray-50 bg-gradient-to-t from-zinc-950 to-zinc-900 shadow-md"
+        className="w-114 py-10 flex-none text-gray-50 bg-gradient-to-t from-zinc-950 to-zinc-900 shadow-md"
       >
         <div className="mb-10 flex justify-start items-center gap-5">
           <div className="bg-[#f0f0f0] rounded-r-full w-fit py-5 pl-7 pr-6 justify-items-center">
@@ -19,7 +19,7 @@ export function App() {
         <Navbar />
       </header>
 
-      <main className="w-full min-w-0 p-10 overflow-y-auto overflow-x-hidden flex flex-col gap-5 shadow-md bg-white/90">
+      <main className="w-full min-w-0 p-20 overflow-y-auto overflow-x-hidden flex flex-col gap-5 shadow-md bg-white/90">
         <Outlet />
       </main>
     </>
