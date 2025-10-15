dbf106b54e9845825c6919b35d4920d941d0caba
rpds2 <rafael.dsilva@sptech.school>
2025-10-15 18:40:36 -0300

    Ajuste B.O. merge

--- src/main/java/com/example/crudObsidiana/repository/EquipamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.crudObsidiana.repository;
-
-import com.example.crudObsidiana.model.Equipamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipamentoRepository extends JpaRepository<Equipamento, Long> {
-
-}
