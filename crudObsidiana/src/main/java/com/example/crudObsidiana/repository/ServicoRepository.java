9c27d9ac0aac12f1201b3732209f388d81ef62a6
Pedro-Lion <pedro.santos033@sptech.school>
2025-10-14 21:13:52 -0300

    Adequação do Backend, Swagger e Cruds

--- crudObsidiana/src/main/java/com/example/crudObsidiana/repository/ServicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crudObsidiana.repository;
-
-import com.example.crudObsidiana.model.Equipamento;
-import com.example.crudObsidiana.model.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository<Servico, Long> {
-
-}
--- src/main/java/com/example/crudObsidiana/repository/ServicoRepository.java
+++ src/main/java/com/example/crudObsidiana/repository/ServicoRepository.java
@@ -1,6 +1,5 @@
 package com.example.crudObsidiana.repository;
 
-import com.example.crudObsidiana.model.Equipamento;
 import com.example.crudObsidiana.model.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
