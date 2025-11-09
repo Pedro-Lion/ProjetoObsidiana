1a5c4732d244445370c9f3a67b380016bad7e678
mikki-unix <mikki.anjos@sptech.school>
2025-11-09 16:57:08 -0300

    [navbar] bootstrap icons e lógica de apenas um drop aberto

--- front-end/src/router.jsx
+++ front-end/src/router.jsx
@@ -5,7 +5,7 @@ import { Erro404 } from "./routes/Erro404";
 import { Login } from "./routes/Login";
 
 import { Home } from "./routes/Home";
-import { Novo } from "./routes/Novo";
+import { CadastroGenerico } from "./routes/CadastroGenerico";
 import { Equipamentos } from "./routes/Equipamentos";
 import { CadastroEquipamentos } from "./routes/CadastroEquipamento";
 import { Servicos } from "./routes/Servicos";
@@ -26,8 +26,8 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/novo/:item",
-        element: <Novo />,
+        path: "/cadastro/:item",
+        element: <CadastroGenerico />,
       },
       {
         path: "/equipamentos",
