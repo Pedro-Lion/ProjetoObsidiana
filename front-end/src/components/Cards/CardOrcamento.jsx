68b36ae86758986ce117dcac9fc2d8671eb14f66
mikki-unix <mikki.anjos@sptech.school>
2025-11-22 16:58:40 -0300

    [card orcamento] inclusão serviços e prop dados sem valores

--- front-end/src/components/Cards/CardOrcamento.jsx
+++ front-end/src/components/Cards/CardOrcamento.jsx
@@ -4,16 +4,32 @@ import { Foto } from "../Foto";
 
 export function CardOrcamento({
   dados = {
-    status: "Confirmado",
-    dataEvento: "2025-11-05",
-    localEvento: "Estúdio Principal",
-    duracaoEvento: 8,
-    descricao: "Gravação de videoclipe publicitário",
-    equipamentos: [{}],
+    id: 1,
+    status: "",
+    dataEvento: "",
+    localEvento: "",
+    duracaoEvento: 0,
+    descricao: "",
+    servicos: [
+      {
+        id: 1,
+        nome: "",
+        valorPorHora: 0,
+      },
+    ],
   },
   onClickEdit,
   onClickDel,
 }) {
+  const formatarValor = new Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  }).format;
+
+  const valorServicos = dados.servicos.reduce((acumulador, atual) => acumulador + atual.valorPorHora, 0);
+
   function formatarData() {
     const dataSemNormalizacao = Intl.DateTimeFormat("pt-br", {
       day: "numeric",
@@ -29,28 +45,23 @@ export function CardOrcamento({
   const dataFormatada = formatarData();
 
   function definirCorStatus() {
-    let cor;
+    const corPorStatus = {
+      confirmado: "oklch(62.7% 0.194 149.214)",
+      pendente: "oklch(68.1% 0.162 75.834)",
+      cancelado: "oklch(50.5% 0.213 27.518)",
+    };
 
-    switch (dados.status.toLowerCase()) {
-      case "confirmado":
-        cor = "oklch(62.7% 0.194 149.214)";
-        break;
-
-      case "pendente":
-        cor = "oklch(68.1% 0.162 75.834)"
-        break;
-      case "cancelado":
-        cor = "oklch(50.5% 0.213 27.518)"
-        break;
-      default:
-        cor = "black";
-        break;
-    }
-
-    return cor;
+    const cor = corPorStatus[dados.status.toLowerCase()];
+    return cor ? cor : "black";
   }
   const corStatus = definirCorStatus();
-  
+
+  const servicos = dados.servicos.map((s) => (
+    <li className="w-full p-3 flex justify-between bg-violet-200 rounded-md text-xl">
+      <span className="font-medium">{s.nome}</span>
+      {formatarValor(s.valorPorHora)}
+    </li>
+  ))
 
   return (
     <div className="w-120 h-160 flex flex-col border rounded-xl">
@@ -58,33 +69,34 @@ export function CardOrcamento({
         <div className="flex justify-between items-center">
           <span className="text-4xl font-medium">{dataFormatada}</span>
           <div className="flex items-center gap-2">
-            <span style={{color: corStatus}}>{dados.status}</span>
-            <div style={{backgroundColor: corStatus}} className={"size-3 rounded-full"}></div>
+            <span style={{ color: corStatus }}>{dados.status}</span>
+            <div
+              style={{ backgroundColor: corStatus }}
+              className={"size-3 rounded-full"}
+            ></div>
           </div>
         </div>
 
-        <ul className="list-disc list-inside">
-          <li>{dados.localEvento}</li>
+        <ul className="list-disc list-inside my-2.5">
+          <li className="mb-1">{dados.localEvento}</li>
           <li>Duração: {dados.duracaoEvento}h</li>
         </ul>
 
-        <p className="mt-3 text-xl">{dados.descricao}</p>
+        <p className="text-xl">{dados.descricao}</p>
       </div>
 
-      <div className="h-full"></div>
-
-      {/* <div className="p-3 text-2xl flex justify-between">
+      <div className="p-3 text-2xl flex justify-between">
         <span>
-          <b>{dados.equipamentos.length} </b>
-          {dados.equipamentos.length > 1 ? "equipamentos" : "equipamento"}
+          <b>{dados.servicos.length} </b>
+          {dados.servicos.length > 1 ? "serviços" : "serviço"}
         </span>
 
-        <span>{formatarValor(valorEquipamentos)}</span>
+        <span>{formatarValor(valorServicos)}</span>
       </div>
 
       <ul className="h-full px-3 flex flex-col gap-3 overflow-y-auto">
-        {equipamentos}
-      </ul> */}
+        {servicos}
+      </ul>
 
       <div className="h-20 p-3 border-t">
         <BotaoPrimario
