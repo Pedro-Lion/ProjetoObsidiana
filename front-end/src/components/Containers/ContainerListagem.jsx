07f600cfe318a96dc55290e1d2822a7b21bd415d
shell-y <shelly.nadudvari@sptech.school>
2025-11-30 18:05:08 -0300

    ajustes no container de listagem de itens

--- front-end/src/components/Containers/ContainerListagem.jsx
+++ front-end/src/components/Containers/ContainerListagem.jsx
@@ -12,98 +12,137 @@ export function ContainerListagem({
   },
   onClickEdit, onClickDel
 }) {
-  return (
-    <div
-      style={{
-        width: "100%",
-        marginBlock: "1rem",
-        backgroundColor: "#f5f3ff",
-        padding: "0rem 0rem 1rem 0rem",
-        borderRadius: "0.5rem",
-      }}
-    >
-      <div className="flex justify-between items-baseline bg-violet-200 px-10 py-1.5 mb-5">
-        <label className="text-slate-700 text-xl font-bold leading-5.5 uppercase">
-          {dados.nome}
-        </label>
+    return (
+        <>
+        <style>
+        {`
+                .container-item-lista{
+                width: 100%;
+                margin-block: 1rem;
+                background-color: #f5f3ff;
+                padding: 0rem 0rem 1rem 0rem;
+                border-radius: 0.5rem;
+                }
 
-        <label className="text-slate-700 text-[1.1rem] w-fit leading-5.5 uppercase">
-          {dados.categoria}
-        </label>
-        <label className="text-slate-700 text-[1.1rem] w-fit leading-5.5">
-          <b>{dados.quantidade}</b> disponíveis
-        </label>
-      </div>
-      <div className="flex flex-row items-center gap-10 my-3 justify-start px-10">
-        <Foto tamanho="6" />
-        <div className="flex flex-row gap-10 justify-between items-start">
-          <div className="flex flex-col gap-3 max-w-[15%]">
-            <label className="text-slate-700 text-[1.1rem] font-medium w-fit max-w-3/5 leading-5.5">
-              Marca
-            </label>
-            <label className="text-slate-700 text-[1.1rem] leading-5.5">
-              {dados.marca}
-            </label>
-          </div>
-          <div className="flex flex-col gap-3 max-w-[15%]">
-            <label className="text-slate-700 text-[1.1rem] font-medium leading-5.5">
-              Modelo
-            </label>
-            <label className="text-slate-700 text-[1.1rem] leading-5.5">
-              {dados.modelo}
-            </label>
-          </div>
-          <div className="flex flex-col gap-3 max-w-[15%]">
-            <label className="text-slate-700 text-[1.1rem] font-medium leading-5.5">
-              NºSerie
-            </label>
-            <label className="text-slate-700 text-[1.1rem] leading-5.5">
-              {dados.numeroSerie}
-            </label>
-          </div>
-          <div className="flex flex-col gap-3 max-w-[15%]">
-            <label className="text-slate-700 text-[1.1rem] font-medium leading-5.5">
-              Valor/hora
-            </label>
-            <label className="text-slate-700 text-[1.1rem] leading-5.5">
-              {dados.valorPorHora}
-            </label>
-          </div>
-          {/* <div className="flex flex-col gap-3 max-w-[15%]">
-            <label className="text-slate-700 text-[1.1rem] font-medium leading-5.5">
-              Diária
-            </label>
-            <label className="text-slate-700 text-[1.1rem] leading-5.5">
-              R$800,00
-            </label>
-          </div>
-          <div className="flex flex-col gap-3 max-w-[25%]">
-            <label className="text-slate-700 text-[1.1rem] font-medium leading-5.5 w-fit max-w-3/5">
-              Observações
-            </label>
-            <label className="text-slate-700 text-[1.1rem] w-auto leading-5.5">
-              Máquina fotográfica específica para gravações diúrnas.
-            </label>
-          </div> */}
-          <div
-            className="h-15 border-l-1 border-violet-200 pl-9
-            flex flex-row gap-9 self-center"
-          >
-            <i
-              className="bi bi-pencil-square
-              text-slate-700 text-3xl self-center
-              cursor-pointer hover:text-indigo-300"
-              onClick={onClickEdit}
-            ></i>
-            <i
-              className="bi bi-trash3
-              text-slate-700 text-3xl self-center
-              cursor-pointer hover:text-indigo-300"
-              onClick={onClickDel}
-            ></i>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+                .estilo-campos{
+                display:flex;
+                flex-direction:column;
+                gap: 0.75rem;
+                max-width:15%;
+                }
+
+                .estilo-conteudo-campo,
+                .estilo-titulo-campo{
+                color: #334155;
+                font-size:1.1rem;
+                line-height: 1.25;
+                width:auto;
+                overflow-wrap: break-word;
+                hyphens: auto;
+                }
+                
+                .estilo-titulo-campo{
+                font-weight:500;
+                }
+
+            @media(max-width: 900px){
+             /*width >= 768px
+             width <= 900px*/
+                .estilo-campos{
+                max-width:10%;
+                }
+                
+                .estilo-conteudo-campo,
+                .estilo-titulo-campo{
+                font-size:0.75em;
+                color:red;
+                }
+            }
+        `}
+            </style>
+            <div className="container-item-lista">
+                <div className="flex flex-row justify-between items-baseline bg-violet-200 px-10 py-1.5 mb-5">
+                    <label className="sm:wrap-anywhere sm:hyphens-auto text-slate-700 text-xl leading-5.5 font-bold uppercase">
+                        {dados.nome}
+                    </label>
+                    <label className="sm:wrap-anywhere sm:hyphens-auto text-slate-700 text-[1.1rem] w-fit leading-5.5 uppercase">
+                        {dados.categoria}
+                    </label>
+                    <label className="sm:wrap-anywhere sm:hyphens-auto text-slate-700 text-[1.1rem] w-fit leading-5.5">
+                        <b>{dados.quantidade}</b> disponíveis
+                    </label>
+                </div>
+                    <div className="flex flex-row justify-between items-start
+                    w-full
+                    px-7
+                    lg:gap-10
+                    ">
+                        <Foto tamanho="6" />
+                        <div className="estilo-campos">
+                            <label className="estilo-titulo-campo w-fit max-w-3/5">
+                                Marca
+                            </label>
+                            <label className="estilo-conteudo-campo">
+                                {dados.marca}
+                            </label>
+                        </div>
+                        <div className="estilo-campos">
+                            <label className="estilo-titulo-campo">
+                                Modelo
+                            </label>
+                            <label className="estilo-conteudo-campo">
+                                {dados.modelo}
+                            </label>
+                        </div>
+                        <div className="estilo-campos">
+                            <label className="estilo-titulo-campo">
+                                NºSerie
+                            </label>
+                            <label className="estilo-conteudo-campo">
+                                {dados.numeroSerie}
+                            </label>
+                        </div>
+                        <div className="estilo-campos">
+                            <label className="estilo-titulo-campo">
+                                Valor/hora
+                            </label>
+                            <label className="estilo-conteudo-campo">
+                                R${dados.valorPorHora}
+                            </label>
+                        </div>
+                        {/* <div className="estilo-campos">
+                            <label className="estilo-titulo-campo">
+                                Diária
+                            </label>
+                            <label className="estilo-conteudo-campo">
+                                R$800,00
+                            </label>
+                        </div>
+                        <div className="flex flex-col gap-3 lg:max-w-[45%] sm:max-w-[25%]">
+                            <label className="estilo-titulo-campo w-fit max-w-3/5">
+                                Observações
+                            </label>
+                            <label className="estilo-conteudo-campo">
+                                Máquina fotográfica específica para gravações diúrnas.
+                            </label>
+                        </div>*/}
+                        <div className="border-l-1 border-violet-200
+                        flex flex-row self-center
+                        lg:pl-9 sm:pl-4
+                        lg:gap-9 sm:gap-4">
+                            <i className="bi bi-pencil-square
+                        text-slate-700 self-center
+                        cursor-pointer hover:text-indigo-300
+                        lg:text-3xl sm:text-2xl"
+                        onClick={onClickEdit}></i>
+                            <i className="bi bi-trash3
+                        text-slate-700 self-center
+                        cursor-pointer hover:text-indigo-300
+                        lg:text-3xl sm:text-2xl"
+                        onClick={onClickDel}></i>
+                        </div>
+                </div>
+            </div>
+        </>
+    )
 }
