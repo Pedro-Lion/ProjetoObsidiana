b403ac828e7a7e68a35a6a6dea4b3ae8e69b1dfe
shell-y <shelly.nadudvari@sptech.school>
2025-12-01 18:19:46 -0300

    removendo pasta back-end

--- back-end/src/main/java/com/example/crudObsidiana/dto/LoginRequestDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.crudObsidiana.dto;
-
-// DTO para receber os dados de login do usuário: email e senha.
-// DTO = "Eu preciso receber essas informações no body da requisição".
-
-public record LoginRequestDTO (String email, String senha) {}
