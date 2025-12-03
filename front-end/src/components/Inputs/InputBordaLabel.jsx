bfc474500b44fa371835e228c64fbe53e7d19a41
mikki-unix <mikki.anjos@sptech.school>
2025-12-02 22:46:03 -0300

    tela de serviço - correções

--- front-end/src/components/Inputs/InputBordaLabel.jsx
+++ front-end/src/components/Inputs/InputBordaLabel.jsx
@@ -1,4 +1,4 @@
-export function InputBordaLabel({ titulo = "", type = "text", placeholder = "Digite aqui", value, className = "w-80", onChange}) {
+export function InputBordaLabel({ titulo = "", type = "text", placeholder = "Digite aqui", value, defaultValue, className = "w-80", onInput}) {
   return (
     <div className={"flex flex-col overflow-x-auto " + className}>
       <label
@@ -12,7 +12,8 @@ export function InputBordaLabel({ titulo = "", type = "text", placeholder = "Dig
         type={type}
         placeholder={placeholder}
         value={value}
-        onChange={onChange}
+        defaultValue={defaultValue}
+        onInput={onInput}
         className="border-indigo-500 text-slate-700
         px-3 py-3 text-[1.1rem] bg-transparent border-1 rounded-lg        
         focus:outline-none placeholder:text-black/25"
