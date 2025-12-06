f599adaaca846062fc72b9b99f038fafe0008353
mikki-unix <mikki.anjos@sptech.school>
2025-12-05 22:43:00 -0300

    input data e hora + teste na dev

---
+++ front-end/src/components/Inputs/InputDataBordaLabel.jsx
@@ -0,0 +1,34 @@
+import Datetime from "react-datetime";
+import "react-datetime/css/react-datetime.css";
+
+export function InputDataBordaLabel({
+  titulo = "",
+  placeholder = "Insira a data e hora",
+  value,
+  defaultValue,
+  className = "w-80",
+  onChange,
+}) {
+  return (
+    <div className={"flex flex-col " + className}>
+      <label
+        className="relative top-3 ml-[0.7rem] px-[0.3rem]
+        text-indigo-500 font-medium text-[1.1rem]
+        bg-white w-fit z-99"
+      >
+        {titulo}
+      </label>
+      <Datetime
+        locale="pt-BR"
+        timeFormat="HH:mm"
+        className="border-indigo-500 text-slate-700
+        px-3 py-3 text-[1.1rem] bg-transparent border-1 rounded-lg"
+        inputProps={{
+          className: "focus:outline-none placeholder:text-black/25",
+          placeholder: placeholder,
+        }}
+        onChange={onChange ? (data) => onChange(data.format()) : undefined}
+      />
+    </div>
+  );
+}
