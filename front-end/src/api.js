459090643a8031bfeb2627b4ac76d9ad1139dcd3
mikki-unix <179178853+mikki-unix@users.noreply.github.com>
2026-04-10 00:53:01 -0300

    front-end | variáveis de ambiente seguras

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL });
+export const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api" });
