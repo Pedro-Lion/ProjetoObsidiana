ed66c48f7ec1c9d2b385c9693e64785664e85755
shell-y <shelly.nadudvari@sptech.school>
2026-04-20 23:30:02 -0300

    tirando 'absolute' do posicionamento da modal

--- front-end/src/components/Modal/Modal.jsx
+++ front-end/src/components/Modal/Modal.jsx
@@ -1,14 +1,16 @@
 export function Modal({ titulo, descricao, children }) {
   return (
     <>
-      <div className="absolute w-full h-full -m-20 bg-black opacity-10"></div>
+      {/* Overlay fixo à viewport, cobre a tela inteira independente do scroll */}
+      <div className="fixed inset-0 bg-black opacity-30 z-40"></div>
 
+      {/* Modal fixo à viewport, sempre centralizado na tela visível */}
       <section
         style={{
           transform: "translate(-50%, -50%)",
         }}
         className="
-          absolute top-1/2 left-1/2 overflow-y-scroll
+          fixed top-1/2 left-1/2 overflow-y-auto z-50
           border-box w-[80%] max-w-220 h-fit min-h-75 p-10
           flex flex-col justify-between
           bg-white rounded-2xl shadow-xl border border-gray-300
